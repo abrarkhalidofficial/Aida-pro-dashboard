61feef3a7b4b0915275cca7b8fc782f6e26ee5eb
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-04-27 23:36:26 +0500

    commit

--- src/screens/DashboardPanel.js
+++ src/screens/DashboardPanel.js
@@ -5,6 +5,31 @@ import LineChart from "../components/LineChart";
 import SpineChart from "../components/SpineChart";
 import BarChart from "../components/BarChart";
 import DoughnutChart from "../components/DoughnutChart";
+import UserPic from "../assets/userPic.png";
+import { Link } from "react-router-dom";
+import { Nav } from "react-bootstrap";
+
+const MessagePanelRow = () => {
+  return (
+    <Nav.Link
+      to="/dashboard/messages"
+      as={Link}
+      className="message__panel__small__row"
+    >
+      <div className="message__panel__samll__content">
+        <img
+          src={UserPic}
+          alt="user-pic"
+          className="message__panel__small__row__img"
+        />
+        <div className="messenger__name" style={{ fontWeight: "800" }}>
+          Francesca Metts
+        </div>
+      </div>
+      <div className="messenger__name">3 Days Ago</div>
+    </Nav.Link>
+  );
+};
 
 export default class DashboardPanel extends React.Component {
   render() {
@@ -20,13 +45,29 @@ export default class DashboardPanel extends React.Component {
           >
             <LineChart />
           </Card>
-          <div style={{ width: "40%" }}>
+          <div className="message__card__col">
             <Card
               heading="Lorem"
               infoHeading="Lorem"
               infoContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam."
             >
-              hello
+              <div className="message__panel__small">
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+                <MessagePanelRow />
+              </div>
             </Card>
           </div>
         </div>
